2b8fe0dcb9e7bffe5d1a9420bc9abf4c7dc5d883
Daniel Antonio <daniel.conte@meumobi.com>
2019-09-12 14:43:44 -0300

    initial commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,5 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'AIzaSyAjFjJJw5um-kDyUpJNmQ_kQyYs7C6xM_k',
-    authDomain: 'firestore-custom-auth-angular.firebaseapp.com',
-    databaseURL: 'https://firestore-custom-auth-angular.firebaseio.com',
-    projectId: 'firestore-custom-auth-angular',
-    storageBucket: '',
-    messagingSenderId: '907228509963',
-    appId: '1:907228509963:web:7d90ed1d7bb77b21'
   }
 };
